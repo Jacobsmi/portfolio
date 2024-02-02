64c8ae81f214c4666def23f4ccaf32af6f828f71
Michael Jacobs <jacobsm84@gmail.com>
2024-02-02 14:39:39 -0500

    remove pure white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
         <div className="bg-slate-800 h-screen w-full p-8 md:p-12 flex flex-col justify-between">
           <div></div>
           <div>
-            <div className="text-3xl sm:text-4xl lg:text-6xl font-semibold text-white">
+            <div className="text-3xl sm:text-4xl lg:text-6xl font-semibold text-stone-200">
               Hi, I&apos;m Michael.
             </div>
-            <div className="text-white sm:text-lg lg:text-xl mt-4">
+            <div className="text-stone-200 sm:text-lg lg:text-xl mt-4">
               Software Engineer, web-enthusiast, golfer, and much more. I love
               bringing functionality to the web, making it look beautiful, and I
               love moving fast.
@@ -20,14 +20,14 @@ export default function Home() {
           <div className="flex items-center justify-between">
             <Link
               href={"https://github.com/Jacobsmi"}
-              className="flex items-center text-white hover:underline"
+              className="flex items-center text-stone-200 hover:underline"
             >
               <FiGithub className="mr-2" /> Github
               <FiArrowUpRight />
             </Link>
             <Link
               href={"https://github.com/Jacobsmi"}
-              className="flex items-center text-white hover:underline"
+              className="flex items-center text-stone-200 hover:underline"
             >
               <FiLinkedin className="mr-2" /> LinkedIn
               <FiArrowUpRight />
