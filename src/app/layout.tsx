f5c29f92d321959de16a791890609c20247aa30a
Michael Jacobs <jacobsm84@gmail.com>
2024-02-02 14:55:24 -0500

    Update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Michael Jacobs",
-  description: "My Portfolio site :)",
+  description: "My Portfolio site",
 };
 
 export default function RootLayout({
